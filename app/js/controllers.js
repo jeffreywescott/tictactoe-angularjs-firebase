c9b1cbce77cc3249bb2b4786766323ef5dbc3890
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-04-24 16:43:58 -0700

    basic win logic in place

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -17,11 +17,70 @@ angular.module('ticTacToe.controllers', ['firebase', 'ngCookies'])
   .controller('GameCtrl', ['$scope', '$routeParams', '$location', '$cookies', 'angularFire', function($scope, $routeParams, $location, $cookies, angularFire) {
     var url = 'https://jeffrey-wescott.firebaseio.com/tictactoe/games';
     var promise = angularFire(url, $scope, 'games');
+    $scope.gameOver = false;
     $scope.username = $cookies.username;
     promise.then(function(games) {
       watchGame($scope, $routeParams);
     });
   }]);
+
+function canMakeMove($scope) {
+  if ($scope.gameOver) return false;
+
+  if ($scope.username === $scope.game.player1 || $scope.username === $scope.game.player2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkForWinner($scope) {
+  var b = $scope.game.board;
+  //--------------
+  // ROW WINS:
+  //--------------
+  // 0,0  0,1  0,2
+  // 1,0  1,1  1,2
+  // 2,0  2,1  2,2
+  //--------------
+  if (b[0][0] === b[0][1] && b[0][1] === b[0][2]) {
+    return b[0][0];
+  } else if (b[1][0] === b[1][1] && b[1][1] === b[1][2]) {
+    return b[1][0];
+  } else if (b[2][0] === b[2][1] && b[2][1] === b[2][2]) {
+    return b[2][0];
+  }
+
+  //--------------
+  // COL WINS:
+  //--------------
+  // 0,0  1,0  2,0
+  // 0,1  1,1  2,1
+  // 0,2  1,2  2,2
+  //--------------
+  if (b[0][0] === b[1][0] && b[1][0] === b[2][0]) {
+    return b[0][0];
+  } else if (b[0][1] === b[1][1] && b[1][1] === b[2][1]) {
+    return b[0][1];
+  } else if (b[0][2] === b[1][2] && b[1][2] === b[2][2]) {
+    return b[0][2];
+  }
+
+  //--------------
+  // DIAG WINS:
+  //--------------
+  // 0,0  1,1  2,2
+  // 0,2  1,1  2,0
+  //--------------
+  if (b[0][0] === b[1][1] && b[1][1] === b[2][2]) {
+    return b[0][0];
+  } else if (b[0][2] === b[1][1] && b[1][1] === b[2][0]) {
+    return b[0][2];
+  } else {
+    // NO WINNER
+    return false;
+  }
+}
 
 function watchGame($scope, $routeParams) {
   $scope.$watch('games', function() {
@@ -35,17 +94,22 @@ function watchGame($scope, $routeParams) {
   });
 
   $scope.mouseOver = function(row, col, $event) {
-    console.log(row + "," + col + ": " + $scope.game.board[row][col]);
+    if (!canMakeMove($scope)) return;
+
     if ($scope.game.board[row][col] == '') {
       $event.target.style.cursor = 'pointer';
     }
   };
 
   $scope.mouseOut = function($event) {
+    if (!canMakeMove($scope)) return;
+
     $event.target.style.cursor = 'default';
   };
 
   $scope.move = function(row, col) {
+    if (!canMakeMove($scope)) return;
+
     if ($scope.game.board[row][col] != '') return;
 
     if ($scope.username === $scope.game.player1) {
@@ -54,7 +118,15 @@ function watchGame($scope, $routeParams) {
       $scope.game.board[row][col] = 'O';
     }
 
-    console.log($scope.username);
+    var winner = checkForWinner($scope);
+    if (winner) {
+      $scope.gameOver = true;
+      if (winner === 'X') {
+        $scope.title = $scope.game.player1 + " won!";
+      } else if (winner === 'O') {
+        $scope.title = $scope.game.player2 + " won!";
+      }
+    }
   };
 }
 
