e6df862278e20a9ef71b9642694a3784b1686b8f
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-04-26 17:29:13 -0700

    trying to deploy to Heroku

---
+++ web.js
@@ -0,0 +1,15 @@
+var static = require('node-static');
+ 
+//
+// Create a node-static server instance to serve the './app' folder
+//
+var file = new(static.Server)('./app');
+var port = process.env.PORT || 5000;
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    });
+}).listen(port);
